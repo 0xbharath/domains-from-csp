b49768f016f2e5eb66dbb3d79d4a6ad834c061c8
synster <you@example.com>
2018-05-24 21:10:20 +0530

    Updated with DNS resolution

--- csp_parser.py
+++ csp_parser.py
@@ -1,6 +1,14 @@
 from __future__ import print_function
-import sys
-import requests
+
+from requests import get, exceptions
+import click
+from socket import gethostbyname, gaierror
+from sys import version_info, exit
+
+if version_info[0] == 2:
+    from urlparse import urlparse
+elif version_info[0] ==3:
+    import urllib.parse.urlsplit as urlparse
 
 import logging
 
@@ -11,20 +19,25 @@ logging.basicConfig(
 
 logger = logging.getLogger('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+__author__  = "Bharath(github.com/yamakira)"
+__version__ = "0.0.1"
+__purpose__ = '''Parse and print domain names from Content Security Policy(CSP) header'''
+
 def get_csp_header(url):
     try:
         logger.info("[+] Fetching headers for {}".format(url))
-        r = requests.get(url)
-    except requests.exceptions.RequestException as e:
+        r = get(url)
+    except exceptions.RequestException as e:
         print(e)
-        sys.exit(1)
+        exit(1)
 
     if 'Content-Security-Policy' in r.headers:
         csp_header = r.headers['Content-Security-Policy']
         return csp_header
     else:
         logger.info("[+] {} doesn't support CSP header".format(url))
-        sys.exit(1)
+        exit(1)
+
 def get_domains(csp_header):
     domains = []
     csp_header_values = csp_header.split(" ")
@@ -36,18 +49,31 @@ def get_domains(csp_header):
             pass
     return domains
 
-def get_url():
-    if len(sys.argv) <= 1:
-        print("Usage: python domain_enum_csp.py <target_url>\nURL format - http(s)://example.com")
-        sys.exit(1)
-    else:
-        return sys.argv[1]
+def resolve_domains(domains):
+    for domain in set(domains):
+        domain = domain.replace('*.','')
+        if domain .startswith('http') or domain.startswith('https'):
+            domain = urlparse(str(domain)).hostname
+        try:
+            ip_address = gethostbyname(domain)
+            print("\033[92m{0:<30} - {1:20}\033[1;m".format(domain,ip_address.rstrip("\n\r")))
+        except gaierror as e:
+            print("\033[93m{0:<30} - {1:20}\033[1;m".format(domain,"No A record exists"),end=''),
+            print(e.message)
+    pass
 
-def main():
-    url = get_url() 
+@click.command()
+@click.argument('url')
+@click.option('--resolve/--no-resolve','-r', default=False,
+                help='Enable/Disable DNS resolution')
+def main(url, resolve): 
     csp_header = get_csp_header(url)
     domains = get_domains(csp_header)
-    for domain in set(domains):
-        print(domain)
+    if resolve == False:
+        for domain in set(domains):
+            print(domain)
+    else:
+        resolve_domains(domains)
+        
 if __name__ == '__main__':
     main()
