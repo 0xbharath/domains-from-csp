9ebe0e676a488bea2ba1e9e8bc9a0d2ea5045c0b
BBerastegui <borjaberastegui@gmail.com>
2019-03-15 20:01:29 +0000

    Some refactoring, option to clean domains.

--- csp_parser.py
+++ csp_parser.py
@@ -7,21 +7,42 @@ from sys import version_info, exit
 
 if version_info[0] == 2:
     from urlparse import urlparse
-elif version_info[0] ==3:
+elif version_info[0] == 3:
     import urllib.parse.urlsplit as urlparse
 
 import logging
+import tldextract
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(message)s"
-    )
+)
 
 logger = logging.getLogger('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-__author__  = "Bharath(github.com/yamakira)"
+__author__ = "Bharath(github.com/yamakira)"
 __version__ = "0.0.1"
 __purpose__ = '''Parse and print domain names from Content Security Policy(CSP) header'''
+
+
+# https://stackoverflow.com/questions/16891340/remove-a-prefix-from-a-string
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text  # or whatever
+
+
+def clean_domains(domains):
+    clean_domains_set = set()
+    for domain in set(domains):
+        ext = tldextract.extract(str(domain))
+        # If subdomain is wildcard or empty
+        if ext[0] in ['*', '']:
+            clean_domains_set.add('.'.join(ext[1:]))
+        else:
+            clean_domains_set.add('.'.join(ext))
+    return clean_domains_set
+
 
 def get_csp_header(url):
     try:
@@ -38,42 +59,52 @@ def get_csp_header(url):
         logger.info("[+] {} doesn't support CSP header".format(url))
         exit(1)
 
-def get_domains(csp_header):
+
+def get_domains(csp_header, clean=False):
     domains = []
     csp_header_values = csp_header.split(" ")
     for line in csp_header_values:
         if "." in line:
-            line = line.replace(";","")
+            line = line.replace(";", "")
             domains.append(line)
         else:
             pass
-    return domains
+    if clean:
+        return clean_domains(domains)
+    else:
+        return domains
+
 
 def resolve_domains(domains):
-    for domain in set(domains):
-        domain = domain.replace('*.','')
-        if domain .startswith('http') or domain.startswith('https'):
-            domain = urlparse(str(domain)).hostname
+    for domain in clean_domains(domains):
         try:
             ip_address = gethostbyname(domain)
-            print("\033[92m{0:<30} - {1:20}\033[1;m".format(domain,ip_address.rstrip("\n\r")))
+            print("\033[92m{0:<30} - {1:20}\033[1;m".format(domain, ip_address.rstrip("\n\r")))
         except gaierror as e:
-            print("\033[93m{0:<30} - {1:20}\033[1;m".format(domain,"No A record exists"),end=''),
+            print("\033[93m{0:<30} - {1:20}\033[1;m".format(domain, "No A record exists"), end=''),
             print(e.message)
     pass
 
+
 @click.command()
-@click.argument('url')
-@click.option('--resolve/--no-resolve','-r', default=False,
-                help='Enable/Disable DNS resolution')
-def main(url, resolve): 
+@click.option('--url', '-u', required=True,
+              help='Url to retrieve the CSP header from.')
+@click.option('--clean/--dirty', '-c', default=False,
+              help='Return domains "cleaned" (without schema and wildcards)')
+@click.option('--resolve/--no-resolve', '-r', default=False,
+              help='Enable/Disable DNS resolution')
+def main(url, resolve, clean):
     csp_header = get_csp_header(url)
-    domains = get_domains(csp_header)
-    if resolve == False:
+    if clean:
+        domains = get_domains(csp_header, clean=True)
+    else:
+        domains = get_domains(csp_header)
+    if resolve:
+        resolve_domains(domains)
+    else:
         for domain in set(domains):
             print(domain)
-    else:
-        resolve_domains(domains)
-        
+
+
 if __name__ == '__main__':
     main()
